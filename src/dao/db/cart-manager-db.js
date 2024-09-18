8ae264a181cba3a5be0785b6f8ce43b5fea47fca
Pablo Nocera <p.nocerar@gmail.com>
2024-09-18 16:59:55 -0300

    se agrega metodos para actualizar y eliminar carrito

--- src/dao/db/cart-manager-db.js
+++ src/dao/db/cart-manager-db.js
@@ -46,6 +46,47 @@ class CartManager {
       console.log("Error al momento de agregar el producto.", error);
     }
   }
+
+  async actualizarProductoEnCarrito(cartId, productId, newQuantity) {
+    try {
+      const carrito = await this.getCarritoById(cartId);
+
+      // Verificar si el producto existe en el carrito
+      const producto = carrito.products.find(
+        (item) => item.product.toString() === productId
+      );
+
+      if (!producto) {
+        console.log("El producto no existe en el carrito.");
+        return null;
+      }
+
+      producto.quantity = newQuantity;
+      carrito.markModified("products");
+      await carrito.save();
+
+      return carrito;
+    } catch (error) {
+      console.log("Error al actualizar el producto en el carrito.", error);
+    }
+  }
+
+  async eliminarProductoDelCarrito(cartId, productId) {
+    try {
+      const carrito = await this.getCarritoById(cartId);
+
+      carrito.products = carrito.products.filter(
+        (item) => item.product.toString() !== productId
+      );
+
+      carrito.markModified("products");
+      await carrito.save();
+
+      return carrito;
+    } catch (error) {
+      console.log("Error al eliminar el producto del carrito.", error);
+    }
+  }
 }
 
 export default CartManager;
